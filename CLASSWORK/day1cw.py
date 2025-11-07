0ac399ac5c23e97a97ad5d61a1ac52aed9647417
Jithin <jithinsajeev134@gmail.com>
2025-11-07 10:54:59 +0530

    Add initial implementation for total volume calculation

---
+++ CLASSWORK/day1cw.py
@@ -0,0 +1,13 @@
+import random
+apple=15.5
+orange=20
+grape=10.25
+total_volume=apple+orange+grape
+print("total volume sold:",total_volume)
+total_volume_int=int(total_volume)
+print("total volume to int:",total_volume_int)
+total_volume_str=str(total_volume)
+print("total volume to string:",total_volume_str)
+ran=random.randint(5,10)
+totalvol=apple+orange+grape+ran
+print("final total volume:",totalvol)
